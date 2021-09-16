3e709bf328f024b401d6107d11b46dcb9159714a
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-16 23:32:11 +0700

    update

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask,jsonify,request
 app = Flask(name)
 CORS(app)
 @app.route('/')
+
+def index():
+    return "<h1>Skripsi-XGBoost-app !!</h1>"
+
 if name == 'main':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
-	
-def index():
-    return "<h1>Skripsi-XGBoost-app !!</h1>"
