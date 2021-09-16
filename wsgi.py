f5e9f4e6b8a112229b86190b007cdb1df1309946
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-17 00:02:30 +0700

    update

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
 from main import app
 
-if name == 'main':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+if __name__ == "__main__":
+		app.run()
