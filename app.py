752e0cc291ef67f5edafae9f6080d8184a822633
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-16 23:30:06 +0700

    update

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ CORS(app)
 if name == 'main':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
+	
 def index():
     return "<h1>Skripsi-XGBoost-app !!</h1>"
